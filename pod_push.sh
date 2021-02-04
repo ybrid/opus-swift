b7138f8ba31d67564fc31ed73f9d7f70a581dcf8
Florian Nowotny <Florian.Nowotny@nacamar.de>
2021-02-04 15:58:05 +0100

    public push to trunk

--- pod_push.sh
+++ pod_push.sh
@@ -1,1 +1,6 @@
-pod repo push Private-CocoaPods YbridOpus.podspec --allow-warnings --verbose
+# public 
+#pod trunk register florian.nowotny@nacamar.de 'Florian Nowotny' --description='macbook pro'
+pod trunk push YbridOpus.podspec --allow-warnings --verbose
+# https://raw.githubusercontent.com/CocoaPods/Specs/e0a445145b695f15f9cc49a91b64a13a3359cbac/Specs/a/e/3/YbridOpus
+# private
+#pod repo push Private-CocoaPods YbridOpus.podspec --allow-warnings --verbose
